9a52edb7807af36372839a4c75d946e230995135
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2023-08-14 23:58:22 +0600

    user section add

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 console.log('Welcome to js');
 
-document.getElementById('submit').addEventListener('click', function(){
+document.getElementById('submit').addEventListener('click', function () {
     const emailFild = document.getElementById('email');
     const passwordFild = document.getElementById('password');
-    
-    if(emailFild.value == 'md.ahadkhanpathan.tct@gmail.com' && passwordFild.value == 'ahadkhan')
-    {
+
+    if (emailFild.value == 'md.ahadkhanpathan.tct@gmail.com' && passwordFild.value == 'ahadkhan') {
+        window.location.href = 'user.html';
         console.log('User is valid');
     }
-    else
-    {
-        console.log('User is not valid');
+    else {
+        alert('Your Email and Password is not Corrrect ! Please Enter Your Valid Email and Password.')
     }
 })
+
